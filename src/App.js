10a7e512e8d2ffcc2318a5b52bd9249c429d7472
Fabian <jujescufabian@gmail.com>
2023-11-01 13:27:08 -0500

    Add props to components to refactor flow of information.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from "react";
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
-function App() {
+const business = {
+  imageSrc:
+    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Bordertown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+const businesses = [business, business, business, business, business, business];
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -11,11 +27,11 @@ function App() {
       </div>
       <SearchBar />
       <div className="business">
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Business from '../Business/Business';
-import businesses from './businesses';
+/*import businesses from './businesses';*/
 import './BusinessList.css'
 
-class BusinessList extends React.Component { 
-    render() {
+
+const BusinessList = (props) => {
         return (
-            <div className="business-list">
-                {businesses.map((business) => {
-                    return <Business business={business} />
-                }) }
-            </div>
-        )
+                <div className="business-list">
+                    {props.businesses.map((business) => {
+                        return <Business business={business} key={business.name} />
+                    }) }
+                </div>
+            );
     }
-}
+
 
 export default BusinessList; 
 
