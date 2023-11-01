25bee0a34926549d4ffe7e7b945a87c5faabe5f8
Fabian <jujescufabian@gmail.com>
2023-11-01 13:25:54 -0500

    Add props for three components to recieve and use each others' data.

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,41 +1,27 @@
 import React from 'react';
 import './Business.css';
 
-const pizzaShop = {
-    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'ITALIAN',
-    rating: 4.5,
-    reviewCount: 90
-}
-
-class Business extends React.Component {
-    render() {
+const Business = ({ business }) => {
         return (
             <div className="business">
                 <div className="image-container">
-                    <img id="restaraunt-img" src={pizzaShop.imageSrc} alt=''/>
+                    <img id="restaraunt-img" src={business.imageSrc} alt=''/>
                 </div>
-                <h2>{pizzaShop.name}</h2>
+                <h2>{business.name}</h2>
                 <div className="business-info">
                     <div className="business-address">
-                        <p className="street">{pizzaShop.address}</p>
-                        <p className="city">{pizzaShop.city}</p>
-                        <p className="state-zip">{`${pizzaShop.state} ${pizzaShop.zipCode}`}</p>
+                        <p className="street">{business.address}</p>
+                        <p className="city">{business.city}</p>
+                        <p className="state-zip">{`${business.state} ${business.zipCode}`}</p>
                     </div>
                 <div className="business-reviews">
-                    <h3 className="category">{pizzaShop.category}</h3>
-                    <h3 className="rating">{`${pizzaShop.rating} stars`}</h3>
-                    <p className="review-count">{`${pizzaShop.reviewCount} reviews`}</p>
+                    <h3 className="category">{business.category}</h3>
+                    <h3 className="rating">{`${business.rating} stars`}</h3>
+                    <p className="review-count">{`${business.reviewCount} reviews`}</p>
                 </div>
                 </div>
             </div>
         );
     }
-}
 
 export default Business;
